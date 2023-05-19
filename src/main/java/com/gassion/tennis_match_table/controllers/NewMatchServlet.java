1501a65e690a04828c6af0e8e68840811b4eb57c
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-19 23:17:14 +0300

    feat : NewMatchServlet is ready

--- src/main/java/com/gassion/tennis_match_table/controllers/NewMatchServlet.java
+++ src/main/java/com/gassion/tennis_match_table/controllers/NewMatchServlet.java
@@ -2,27 +2,38 @@ package com.gassion.tennis_match_table.controllers;
 
 import com.gassion.tennis_match_table.Util.ValidateUtil;
 import com.gassion.tennis_match_table.Util.exceptions.RequestParamException;
+import com.gassion.tennis_match_table.entities.LocalEntities.MatchDTO;
+import com.gassion.tennis_match_table.entities.Match;
+import com.gassion.tennis_match_table.entities.Player;
+import com.gassion.tennis_match_table.entities.factories.PlayerFactory;
+import com.gassion.tennis_match_table.service.OngoingMatchesService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
-import java.util.Map;
+import java.util.List;
+import java.util.UUID;
 
 @WebServlet(name = "NewMatchServlet", value = "/new-match")
 public class NewMatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //response.getWriter().println("/new-match");
         response.sendRedirect(request.getContextPath() + "/new_match.jsp");
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
         try {
             ValidateUtil.newMatchRequestValidate(request);
+            List<Player> players = PlayerFactory.getPlayersFromRequest(request);
 
-        } catch (RequestParamException e) {
+            UUID newMatchKey = OngoingMatchesService.addMatch(players);
+            MatchDTO newMatch = OngoingMatchesService.getMatchDTO(newMatchKey);
+
+            request.setAttribute("match", newMatch);
+            response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + newMatchKey);
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
