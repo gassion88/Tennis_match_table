dd3ffacde42c0742bb69e6bba4f4f2f9db9ab89c
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-20 22:23:30 +0300

    refactor : delete uninset dependinsies

--- src/main/java/com/gassion/tennis_match_table/view/MatchScoreView.java
+++ src/main/java/com/gassion/tennis_match_table/view/MatchScoreView.java
@@ -1,8 +1,6 @@
 package com.gassion.tennis_match_table.view;
 
 import com.gassion.tennis_match_table.entities.DTO.DTO;
-import com.gassion.tennis_match_table.entities.MatchModel.MatchModel;
-import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MatchScoreView extends View{
-    private static final Gson GSON = new Gson();
     String page = "/match_scores.jsp";
     @Override
     public void display(HttpServletRequest request, HttpServletResponse response, DTO dto) throws IOException, ServletException {
